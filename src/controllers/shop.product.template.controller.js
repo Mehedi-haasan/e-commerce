27bb37096442f755a9c3fd19d86d22c040d3124f
mazharulsabbir <mazharulsabbir2@gmail.com>
2023-10-28 09:36:39 +0600

    [FEAT] product template and variant create api

--- src/controllers/shop.product.template.controller.js
+++ src/controllers/shop.product.template.controller.js
@@ -1,4 +1,5 @@
 const db = require("../models");
+const sequelize = db.sequelize;
 const ProductTemplate = db.productTemplate;
 const ProductCategory = db.productCategory;
 const ProductTemplateAttribute = db.productTemplateAttribute;
@@ -77,7 +78,7 @@ function generateCombinations(attributes) {
     return combinations;
 }
 
-exports.createProduct = (req, res) => {
+exports.createProduct = async (req, res) => {
     const body = req.body;
     if (!body) {
         return res.status(400).send({
@@ -85,6 +86,8 @@ exports.createProduct = (req, res) => {
             message: "Request body cannot be empty."
         });
     }
+
+    const transaction = await sequelize.transaction();
 
     var productBody = {
         name: body.name,
@@ -100,75 +103,81 @@ exports.createProduct = (req, res) => {
 
     const variantCombinations = generateCombinations(body.attributes)
 
-    ProductTemplate.create(productBody)
-        .then(template => {
-            var attributeIds = []
-            for (let i = 0; i < body.attributes.length; i++) {
-                const attributeId = body.attributes[i]['attr_id'];
-                attributeIds.push({
-                    attr_id: attributeId,
-                    tmpl_id: template.id,
+    try {
+        // create product template
+        const template = await ProductTemplate.create(productBody);
+        var attributeIds = []
+        for (let i = 0; i < body.attributes.length; i++) {
+            const attributeId = body.attributes[i]['attr_id'];
+            attributeIds.push({
+                attr_id: attributeId,
+                tmpl_id: template.id,
+                active: true,
+            });
+        }
+
+        // create product template attributes
+        const attrs = await ProductTemplateAttribute.bulkCreate(attributeIds);
+        var attributeValues = []
+        for (let i = 0; i < attrs.length; i++) {
+            const attr = attrs[i];
+            // search on body.attributes dict to get matched with attr.attr_id
+            const matchedItems = body.attributes.filter(item => item.attr_id == attr.attr_id).reduce((arrays, obj) => {
+                obj.values.forEach(value_id => {
+                    arrays.push(value_id);
+                });
+                return arrays;
+            }, []);
+
+            matchedItems.forEach(attributeValue => {
+                attributeValues.push({
+                    value_id: attributeValue,
+                    product_tmpl_attr_id: attr.id,
                     active: true,
                 });
-            }
+            });
+        }
 
-            ProductTemplateAttribute.bulkCreate(attributeIds).then(attrs => {
-                var attributeValues = []
-                for (let i = 0; i < attrs.length; i++) {
-                    const attr = attrs[i];
-                    // search on body.attributes dict to get matched with attr.attr_id
-                    const matchedItems = body.attributes.filter(item => item.attr_id == attr.attr_id).reduce((arrays, obj) => {
-                        obj.values.forEach(value_id => {
-                            arrays.push(value_id);
-                        });
-                        return arrays;
-                    }, []);
+        // create product template attribute values
+        await ProductTemplateAttributeValue.bulkCreate(attributeValues);
 
-                    matchedItems.forEach(attributeValue => {
-                        attributeValues.push({
-                            value_id: attributeValue,
-                            product_tmpl_attr_id: attr.id,
-                            active: true,
-                        });
-                    });
-                }
+        // create product variants
+        var variants = [];
+        for (let i = 0; i < variantCombinations.length; i++) {
+            var myTemplateData = productBody;
+            myTemplateData.template_id = template.id;
+            variants.push(myTemplateData);
+        }
 
-                ProductTemplateAttributeValue.bulkCreate(attributeValues).then(values => {
-                    var variants = [];
-                    for (let i = 0; i < variantCombinations.length; i++) {
-                        var myTemplateData = productBody;
-                        myTemplateData.template_id = template.id;
-                        variants.push(myTemplateData);
-                    }
-
-                    ProductVariant.bulkCreate(variants).then(variantItems => {
-                        var variantAttributeValues = [];
-                        for (let i = 0; i < variantItems.length; i++) {
-                            const variantItem = variantItems[i];
-                            const variantCombination = variantCombinations[i];
-                            variantCombination.forEach(combination => {
-                                variantAttributeValues.push({
-                                    variant_id: variantItem.id,
-                                    attr_value_id: combination.value,
-                                    attr_id: combination.attr_id,
-                                    active: true,
-                                });
-                            });
-                        }
-
-                        ProductVariantAttributeValue.bulkCreate(variantAttributeValues).then(_ => {
-                            res.send({
-                                success: true,
-                                message: "Record Created successfully!"
-                            });
-                        });
-                    });
+        // create product variant attribute values
+        const variantItems = ProductVariant.bulkCreate(variants);
+        var variantAttributeValues = [];
+        for (let i = 0; i < variantItems.length; i++) {
+            const variantItem = variantItems[i];
+            const variantCombination = variantCombinations[i];
+            variantCombination.forEach(combination => {
+                variantAttributeValues.push({
+                    variant_id: variantItem.id,
+                    attr_value_id: combination.value,
+                    attr_id: combination.attr_id,
+                    active: true,
                 });
-            })
-        })
-        .catch(err => {
-            res.status(500).send({ success: false, message: err.message });
+            });
+        }
+
+        await ProductVariantAttributeValue.bulkCreate(variantAttributeValues);
+
+        // commit transaction
+        await transaction.commit();
+
+        res.send({
+            success: true,
+            message: "Record Created successfully!"
         });
+    } catch (err) {
+        await transaction.rollback();
+        res.status(500).send({ success: false, message: err.message });
+    }
 };
 
 exports.updateProduct = (req, res) => {
