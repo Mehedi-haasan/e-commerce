36ee2c97a235e09521aa523fd09c8607ca097060
Md Mazharul Islam <mazharulsabbir2@gmail.com>
2023-11-24 07:09:24 +0600

    []

--- Server.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-const app = require('./src/app');
-
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
